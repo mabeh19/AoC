645797c06c9ad24002ee96eab378ab620723fa9b
mabeh19 <mathiasibehrens@gmail.com>
2023-12-06 22:49:41 +0100

    Whoops, had added rust target folders

--- 2023/day5/day5r/target/release/deps/day5r-f507accff997ca8e.d
+++
@@ -1,6 +0,0 @@
-/home/mathias/Documents/AoC/2023/day5/day5r/target/release/deps/day5r-f507accff997ca8e: src/main.rs src/../input.txt
-
-/home/mathias/Documents/AoC/2023/day5/day5r/target/release/deps/day5r-f507accff997ca8e.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- 2023/day6/day6rs/target/release/day6rs.d
+++
@@ -1,1 +0,0 @@
-/home/mathias/Documents/AoC/2023/day6/day6rs/target/release/day6rs: /home/mathias/Documents/AoC/2023/day6/day6rs/input.txt /home/mathias/Documents/AoC/2023/day6/day6rs/src/main.rs
